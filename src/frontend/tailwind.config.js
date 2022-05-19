d70cf22e693ba41e7bf8df857630482231398fb3
Clint J Edwards <clint.j.edwards@gmail.com>
2022-05-19 05:53:35 -0400

    ran into more wasm issues

--- src/frontend/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  darkMode: "class", // Determines weather dark mode is set by the operating system automatically or by a class.
-  content: ["./*.rs", "./**/*.rs", "./index.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
